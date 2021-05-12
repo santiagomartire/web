fb3c105e6ff64dc8890e7e62fef07a0d74b686cb
santiagomartire <santiagomartire@gmail.com>
2021-05-12 16:00:53 -0300

    generico php

--- email.php
+++ email.php
@@ -25,8 +25,9 @@ try {
     $mail->addAddress('ambiente@fundacionreciduca.org.ar');     // Mail del destinatario
  
     $mail->isHTML(true);
-    $mail->Subject = 'Alta Cliente de: '.$_POST['input_nombrefantasia'];  // Asunto del mensaje
-    $mail->Body    = 'Nfantasia: '.$_POST['input_nombrefantasia'].'<br>Rsocial: '.$_POST['input_razonsocial'].'<br>Cuit: '.$_POST['input_cuit'].'<br>Direccion: '.$_POST['input_direccion'].'<br>Ciudad: '.$_POST['input_ciudad'].'<br>Provincia: '.$_POST['input_provinica'].'<br>Email: '.$_POST['input_email'].'<br>Celular: '.$_POST['input_celular'].'<br>Ncontacto: '.$_POST['input_nombrecontacto'].'<br>Cargo: '.$_POST['input_cargo'];    // Contenido del mensaje (acepta HTML)
+    $mail->Subject = 'Here is the subject';
+    $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
  
     $mail->send();
     header("location: gracias.html");
